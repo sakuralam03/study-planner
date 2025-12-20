152e458480656cab197037717180c2ac33b4f671
Sakura Lam <1007871@mymail.sutd.edu.sg>
2025-12-20 15:52:56 +0800

    twenty

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,13 @@
-// db.js
-require('dotenv').config(); // only needed locally, Vercel injects env automatically
-const { MongoClient } = require("mongodb");
-
 const uri = process.env.MONGO_URI;
 let client;
 let db;
 
 async function connectDB() {
   if (!client) {
-    console.log("Connecting with URI:", uri);
     client = new MongoClient(uri);
     await client.connect();
-    db = client.db(process.env.DB_NAME || "studyPlanner"); // uses DB_NAME from env
+    db = client.db(); // uses DB from URI
     console.log("✅ Connected to MongoDB:", db.databaseName);
   }
   return db;
 }
-
-module.exports = connectDB;
